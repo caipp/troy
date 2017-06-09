78178850b7d0fb06097ff0a59d960e676aa36312
git <git>
2017-06-09 18:24:39 +0800

    test

--- src/main/java/com/troy/config/Swagger2.java
+++ src/main/java/com/troy/config/Swagger2.java
@@ -30,8 +30,8 @@ public class Swagger2 {
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("Spring Boot中使用Swagger2构建RESTful APIs")
-                .description("更多Spring Boot相关文章请关注：http://blog.didispace.com/")
-                .termsOfServiceUrl("http://blog.didispace.com/")
+                .description("更多Spring Boot相关文章请关注")
+                .termsOfServiceUrl("")
                 .version("1.0")
                 .build();
     }
