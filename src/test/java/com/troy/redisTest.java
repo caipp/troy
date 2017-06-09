8c964b72dca575bea563e9323eec75ab22f3c7e3
git <git>
2017-06-09 15:15:41 +0800

    test

--- src/test/java/com/troy/redisTest.java
+++ src/test/java/com/troy/redisTest.java
@@ -10,7 +10,7 @@ public class redisTest {
     public static void main(String args[]){
         Jedis test = new Jedis("139.196.94.27",6379);
         test.set("cpp","hello world");
-//        test.del("cpp");
+        test.del("cpp");
         String hello = test.get("cpp");
         System.out.println(hello);
     }
